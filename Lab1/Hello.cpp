6da5d388df1b6d311be08c07f5c7f2e1d6c891e4
johnNull <nullj27@gmail.com>
2016-03-28 00:58:05 -0400

    Basic c++ syntax

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Hello.h"
+using namespace std;
+
+void Hello::printHello(){
+	cout << "Hello from the Hello Class!" << endl;	
+}
---
+++ Lab1/lab1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Hello.h"
+using namespace std;
+
+int main(){
+	cout << "Hello World!" << endl;
+	cout << "C++ Data Structures!" << endl;
+	Hello hello;
+	hello.printHello();
+}
